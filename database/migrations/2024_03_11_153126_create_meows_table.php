84d1391de3604374b1fa1da32b1b65a21a8905d8
maxime.polese <maxime.polese@le-campus-numerique.fr>
2024-03-12 11:48:45 +0100

    Foreign Key ok

--- database/migrations/2024_03_11_153126_create_meows_table.php
+++ database/migrations/2024_03_11_153126_create_meows_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,6 +14,8 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('content', 300);
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
--- database/migrations/2024_03_12_150705_create_comments_table.php
+++ database/migrations/2024_03_12_150705_create_comments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->string('content', 300);
             $table->timestamps();
+            $table->foreignUuid('meow_id')->references('id')->on('meows');
         });
     }
 
