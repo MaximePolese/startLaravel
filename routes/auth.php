dd0a0e3b2a4ad03398abeabcc7091e866c50a7f6
maxime.polese <maxime.polese@le-campus-numerique.fr>
2024-03-14 11:53:51 +0100

    suppression route meows

--- routes/auth.php
+++ routes/auth.php
@@ -57,5 +57,5 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
-    Route::resource('/meows', MeowController::class);
+//    Route::resource('/meows', MeowController::class);
 });
